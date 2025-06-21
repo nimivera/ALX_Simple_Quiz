cf91f8856ac086a1b42e5504b3ed942dcff7aa3b
nimivera <etimv0524@gmail.com@example.com>
2025-06-21 22:52:34 +0000

    Updated quiz.js file

--- quiz.js
+++ quiz.js
@@ -1,16 +1,13 @@
-function checkAnswer() {Add commentMore actions
-  const correctAnswer = '4' 
-  let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
-
-  const feedback = document.getElementById("feedback");
+function checkAnswer() {
+  const correctAnswer = "4";
+  const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+  const feedbackElement = document.getElementById('feedback');
 
   if (userAnswer === correctAnswer) {
-      feedback.textContent = "Correct! Well done.";
+    feedbackElement.textContent = "Correct! Well done.";
   } else {
-      feedback.textContent = "That's incorrect. Try again!";
+    feedbackElement.textContent = "That's incorrect. Try again!";
   }
 }
 
-
-const submitButton = document.getElementById("submit-answer");
-submitButton.addEventListener("click", checkAnswer);
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
